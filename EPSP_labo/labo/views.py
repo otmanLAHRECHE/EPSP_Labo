38d8e325e8a8577a21022870a2717b824b33313b
Otman LAHRECHE <laotman.47@gmail.com>
2022-12-18 19:02:47 +0100

    updates

--- EPSP_labo/labo/views.py
+++ EPSP_labo/labo/views.py
@@ -531,15 +531,15 @@ def updateTest(request, id):
     if request.method == 'POST' and request.user.is_authenticated:
         
 
-        examen_test_id = request.data.pop('exam_test_id')
+        resultat_test = request.data.pop('resultat_test')
         id = request.data.pop('id')
 
-        examen_test = ExamenTestes.objects.get(id = examen_test_id)
 
         Test_to_update = TestDetails.objects.get(id = id)
         
-        if not Test_to_update.examen_test == examen_test:
-            Test_to_update.examen_test = examen_test
+        if not Test_to_update.resultat_test == resultat_test:
+            Test_to_update.resultat_test = resultat_test
+        
         
         Test_to_update.save()
         
@@ -581,3 +581,189 @@ def getTestResultOfExam(request, id):
     else :
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
+
+
+
+@api_view(['GET'])
+def getAllResults(request):
+    if request.method == 'GET' and request.user.is_authenticated:
+        queryset = Examen.objects.all()
+
+        source_serial = ExemenSerializer(queryset, many=True)
+
+        return Response(status=status.HTTP_200_OK,data=source_serial.data)
+                
+    else :
+        return Response(status=status.HTTP_401_UNAUTHORIZED) 
+
+
+@api_view(['GET'])
+def getSelectedExemen(request, id):
+    if request.method == 'GET' and request.user.is_authenticated:
+        queryset = Examen.objects.get(id = id)
+
+        source_serial = ExemenSerializer(queryset)
+
+        return Response(status=status.HTTP_200_OK,data=source_serial.data)
+                
+    
+    else :
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+
+@api_view(['POST'])
+def createNewExemen(request):
+    if request.method == 'POST' and request.user.is_authenticated:
+
+        no_enregistrement = request.data.pop('no_enregistrement')
+        patient_first_name = request.data.pop('patient_first_name')
+        patient_last_name = request.data.pop('patient_last_name')
+        date_p = request.data.pop('date_prelevement')
+        date_ns = request.data.pop('patient_birth_day')
+        patient_genre = request.data.pop('patient_genre')
+        doctor_send_from = request.data.pop('doctor_send_from')
+        inf_prelevement_id = request.data.pop('inf_prelevement_id')
+        exm_type = request.data.pop('exm_type')
+        test_seen = request.data.pop('test_seen')
+        result_ready = request.data.pop('result_ready')
+
+        inf_prelevement = InfPrileve.objects.get(id=inf_prelevement_id)
+
+        date_prelevement = date_p.split("/")
+        patient_birth_day = date_ns.split("/")
+
+        d_p = date_prelevement[0]
+        m_p = date_prelevement[1]
+
+        d_ns = patient_birth_day[0]
+        m_ns = patient_birth_day[1]
+
+        if d_p[0] == '0':
+            d_p.replace('0','',1)
+        if m_p[0] == '0':
+            m_p.replace('0','',1)
+        if d_ns[0] == '0':
+            d_ns.replace('0','',1)
+        if m_ns[0] == '0':
+            m_ns.replace('0','',1)
+
+        date_prelevement[0] = d_p
+        date_prelevement[1] = m_p
+        patient_birth_day[0] = d_ns
+        patient_birth_day[1] = m_ns
+
+        
+        date_prelevement = datetime.date(int(date_prelevement[2]), int(date_prelevement[1]), int(date_prelevement[0]))
+        patient_birth_day = datetime.date(int(patient_birth_day[2]), int(patient_birth_day[1]), int(patient_birth_day[0]))
+
+
+        source = Examen.objects.create(no_enregistrement = no_enregistrement, patient_first_name = patient_first_name, patient_last_name=patient_last_name, patient_birth_day=patient_birth_day, patient_genre=patient_genre, doctor_send_from=doctor_send_from, date_prelevement=date_prelevement, inf_prelevement=inf_prelevement, exm_type=exm_type, test_seen=test_seen, result_ready=result_ready)
+
+        if source.id is not None:
+            return Response(status=status.HTTP_201_CREATED, data = {"id_examen":source.id})
+        else:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+@api_view(['POST'])
+def updateExemen(request, id):
+    if request.method == 'POST' and request.user.is_authenticated:
+        no_enregistrement = request.data.pop('no_enregistrement')
+        patient_first_name = request.data.pop('patient_first_name')
+        patient_last_name = request.data.pop('patient_last_name')
+        date_p = request.data.pop('date_prelevement')
+        date_ns = request.data.pop('patient_birth_day')
+        patient_genre = request.data.pop('patient_genre')
+        doctor_send_from = request.data.pop('doctor_send_from')
+        inf_prelevement_id = request.data.pop('inf_prelevement_id')
+        exm_type = request.data.pop('exm_type')
+
+        inf_prelevement = InfPrileve.objects.get(id=inf_prelevement_id)
+
+        date_prelevement = date_p.split("/")
+        patient_birth_day = date_ns.split("/")
+
+        d_p = date_prelevement[0]
+        m_p = date_prelevement[1]
+
+        d_ns = patient_birth_day[0]
+        m_ns = patient_birth_day[1]
+
+        if d_p[0] == '0':
+            d_p.replace('0','',1)
+        if m_p[0] == '0':
+            m_p.replace('0','',1)
+        if d_ns[0] == '0':
+            d_ns.replace('0','',1)
+        if m_ns[0] == '0':
+            m_ns.replace('0','',1)
+
+        date_prelevement[0] = d_p
+        date_prelevement[1] = m_p
+        patient_birth_day[0] = d_ns
+        patient_birth_day[1] = m_ns
+
+        
+        date_prelevement = datetime.date(int(date_prelevement[2]), int(date_prelevement[1]), int(date_prelevement[0]))
+        patient_birth_day = datetime.date(int(patient_birth_day[2]), int(patient_birth_day[1]), int(patient_birth_day[0]))
+
+        examen_to_update = Examen.objects.get(id = id)
+
+        if not examen_to_update.no_enregistrement == no_enregistrement:
+            examen_to_update.no_enregistrement = no_enregistrement
+        if not examen_to_update.patient_first_name == patient_first_name:
+            examen_to_update.patient_first_name = patient_first_name
+        if not examen_to_update.patient_last_name == patient_last_name:
+            examen_to_update.patient_last_name = patient_last_name
+        
+        if not examen_to_update.patient_genre == patient_genre:
+            examen_to_update.patient_genre = patient_genre
+        
+        if not examen_to_update.doctor_send_from == doctor_send_from:
+            examen_to_update.doctor_send_from = doctor_send_from
+        
+        if not examen_to_update.exm_type == exm_type:
+            examen_to_update.exm_type = exm_type
+        
+        if not examen_to_update.inf_prelevement.full_name == inf_prelevement.full_name:
+            examen_to_update.inf_prelevement = inf_prelevement
+        
+        if not examen_to_update.date_prelevement == date_prelevement:
+            examen_to_update.date_prelevement = date_prelevement
+            
+        if not examen_to_update.patient_birth_day == patient_birth_day:
+            examen_to_update.patient_birth_day = patient_birth_day
+        
+        examen_to_update.save()
+        
+        return Response(status=status.HTTP_200_OK, data = {"status":"Exemen updated"})
+
+
+@api_view(['DELETE'])
+def deleteExemen(request, id):
+    if request.method == 'DELETE' and request.user.is_authenticated:
+        Examen.objects.filter(id=id).delete()
+        return Response(status=status.HTTP_200_OK, data = {"status":"Exemen deleted"})
+
+
+
+@api_view(['GET'])
+def getAllExamenOfMonth(request, month, year):
+    if request.method == 'GET' and request.user.is_authenticated:
+
+        range = monthrange(year, month)
+
+        date_start = datetime.date(year , month, 1)
+        date_end = datetime.date( year, month, range[1])
+
+        queryset = Examen.objects.filter(date_prelevement__gte=date_start, date_prelevement__lte=date_end).order_by("-date_prelevement")
+
+        source_serial = ExemenSerializer(queryset, many=True)
+
+        return Response(status=status.HTTP_200_OK,data=source_serial.data)
+                
+    else :
+        return Response(status=status.HTTP_401_UNAUTHORIZED) 
